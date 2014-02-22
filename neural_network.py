b64712ba2ca27bc70c0fe68ab4e6584f677a935f
Max <max@recoset.com>
2014-02-21 20:34:01 -0500

    a starting point

---
+++ neural_network.py
@@ -0,0 +1,13 @@
+import theano.tensor as T
+import theano
+
+class Layer(object):
+	def __init__(self):
+		# initialize weights
+		# define size
+		pass
+
+class NeuralNetwork(object):
+	def __init__(self):
+		# put the layers together 
+		pass
