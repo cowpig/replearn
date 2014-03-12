09cca193545f355803db3c550fa87643856e78c4
Max <max@recoset.com>
2014-03-12 11:07:41 -0400

    testing AA

--- denoising_autoencoder.py
+++ denoising_autoencoder.py
@@ -7,24 +7,26 @@ from sklearn.preprocessing import scale
 data = loader.generate_play_set()
 np.random.shuffle(data)
 
-n_train = 500
+n_train = 10000
 
 one_hots = np.array([line[0][0] for line in data[:n_train]])
 infos = np.array([line[0][1] for line in data[:n_train]])
-# scale infos columns 0 and 15?
-raws = np.array([line[0][2] for line in data[:n_train]])
-# scale(raws, axis=1, copy=False)
+infos[0] /= np.max(infos[0])
+infos[15] /= np.max(infos[15])
+raws = np.array([line[0][2] for line in data[:n_train]], dtype=np.float64)
+raws = (raws - np.min(raws)) / (np.max(raws) - np.min(raws))
 
-inputs = theano.shared(np.concatenate((one_hots, infos, raws), axis=1))
-
-labels = np.array(line[1] for line in data[n_train])
+input_matrix = np.concatenate((one_hots, infos, raws), axis=1)
+inputs = theano.shared(input_matrix)
 
 ##############
 # autoencoder
 
 HIDDEN_UNITS = 500
+LEARNING_RATE = 0.1
+CORRUPTION = 0.3
 
-n_in = np.shape(inputs)[0]
+n_in = np.shape(input_matrix)[0]
 n_hidden = HIDDEN_UNITS
 
 # initialization of weights as suggested in theano tutorials
@@ -35,8 +37,36 @@ initialized_W = np.asarray(np.random.uniform(
                             dtype=np.float64)
 
 W = theano.shared(initialized_W, 'W')
-W_out = W.T
 
 b_in = theano.shared(np.zeros(n_in), 'b_in')
 b_hidden = theano.shared(np.zeros(n_hidden), 'b_hidden')
 
+x = T.dmatrix('x')
+
+active_hidden = T.nnet.sigmoid(T.dot(x, W) + b_in)
+output = T.nnet.sigmoid(T.dot(active_hidden, W.T) + b_hidden)
+
+entropy = -T.sum(x * T.log(output) + (1 - x) * T.log(1 - output), axis=1)
+cost = T.mean(entropy)
+
+parameters = [W, b_in, b_hidden]
+gradients = T.grad(cost, parameters)
+
+updates = []
+for param, grad in zip(parameters, gradients):
+    updates.append((param, param - LEARNING_RATE * grad))
+
+train_step = theano.function([x], cost, updates=updates)
+
+def epoch(batch_size):
+    i=0
+    costs = []
+    while i < n_train + batch_size:
+        costs.append(train_step(inputs[i:i+batch_size, :]))
+
+    return costs
+
+while True:
+    cost = epoch(1000)
+    print "costs: {}".format(cost)
+    print "avg: {}".format(np.mean(cost))
